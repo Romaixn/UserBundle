3e9923512e0479fd0b49a36b8073554a2021ce10
Romain Herault <romain.herault99@gmail.com>
2019-11-27 16:15:23 +0100

    :building_construction: adding base directory structure

---
+++ RheraultUserBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Rherault\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class RheraultUserBundle 
+ * @package Rherault\UserBundle 
+ */
+class RheraultUserBundle extends Bundle 
+{
+  
+}
