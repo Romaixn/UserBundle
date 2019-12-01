be4305312e82ff0b6ff8bab4ac0938248a73befd
Romain HERAULT <romain.herault99@gmail.com>
2019-12-01 23:04:37 +0100

    :sparkles: introducing new SecurityController

--- Controller/SecurityController.php
+++ Controller/SecurityController.php
@@ -2,6 +2,7 @@
 
 namespace Rherault\UserBundle\Controller;
 
+use Rherault\UserBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -43,7 +44,7 @@ class SecurityController extends AbstractController
      */
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder) : Response 
     {
-        if($request->isMethod('POST')) {
+        if ($request->isMethod('POST')) {
             $user = new User();
 
             $user->setUsername($request->request->get('username'));
@@ -60,4 +61,95 @@ class SecurityController extends AbstractController
 
         return $this->render('security/register.html.twig');
     }
+
+    /**
+     * @Route("/forgottenPassword", name="rherault_userbundle_forgotten_password")
+     * 
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $encoder
+     * @param Swift_Mailer $mailer
+     * @param TokenGeneratorInterface $tokenGenerator
+     * 
+     * @return RedirectResponse|Response
+     */
+    public function forgottenPassword(Request $request, UserPasswordEncoderInterface $encoder, \Swift_Mailer $mailer, TokenGeneratorInterface $tokenGenerator) : Response 
+    {
+        if ($request->isMethod('POST')) {
+            $email = $request->request->get('email');
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
+
+            if ($user === null) {
+                $this->addFlash('danger', 'Unknown email adress');
+                return $this->redirectToRoute('index');
+            }
+
+            $token = $tokenGenerator->generateToken();
+
+            try {
+                $user->setResetToken($token);
+                $entityManager->flush();
+            } catch(\Exception $e) {
+                $this->addFlash('warning', $e->getMessage());
+                return $this->redirectToRoute('index');
+            }
+
+            $url = $this->generateUrl('app_reset_password', ['token' => $token], UrlGeneratorInterface::ABSOLUTE_URL);
+
+            $message = (new \Swift_Message('Forgotten password'))
+                ->setFrom('romain@rherault.fr')
+                ->setTo($user->getEmail())
+                ->setBody(
+                    "Here is the token to reset your password : " . $url,
+                    "text/html"
+                );
+
+            $mailer->send($message);
+
+            $this->addFlash('notice', 'Mail sent');
+
+            return $this->redirectToRoute('index');
+        }
+
+        return $this->render('security/forgotten_password.html.twig');
+    }
+
+    /**
+     * @Route("/reset_password/{token}", name="rherault_userbundle_reset_password")
+     * 
+     * @param Request $request
+     * @param string $token
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * 
+     * @return RedirectResponse|Response
+     */
+    public function resetPassword(Request $request, string $token, UserPasswordEncoderInterface $passwordEncoder) : Response 
+    {
+        if ($request->isMethod('POST')) {
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $user = $entityManager->getRepository(User::class)->findOneBy(['resetToken' => $token]);
+
+            if ($user === null) {
+                $this->addFlash('danger', 'Unknown token');
+                return $this->redirectToRoute('index');
+            }
+
+            $user->setResetToken(null);
+
+            $user->setPassword($passwordEncoder->encodePassword($user, $request->request->get('password')));
+            $entityManager->flush();
+
+            $this->addFlash('notice', 'Updated password');
+
+            return $this->redirectToRoute('index');
+
+        } else {
+            return $this->render('security/reset_password.html.twig', [
+                'token' => $token
+            ]);
+        }
+    }
+
 }
